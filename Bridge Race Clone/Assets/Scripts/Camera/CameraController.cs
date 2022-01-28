7fdfe9871e07ca558a98066997fc88152e6a384f
Bekir Tuncer <bekirtuncer97@gmail.com>
2022-01-28 21:46:28 +0300

    -edit camera system

--- Bridge Race Clone/Assets/Scripts/Camera/CameraController.cs
+++ Bridge Race Clone/Assets/Scripts/Camera/CameraController.cs
@@ -10,9 +10,14 @@ namespace BridgeRace
 
         public Transform Target;
 
+        private void Start()
+        {
+            transform.SetParent(null);
+        }
+
         private void LateUpdate()
         {
-            Vector3 targetPosition = Target.position + _cameraControllerSettings.Offset;
+            Vector3 targetPosition = new Vector3(0, 0, Target.position.z) + _cameraControllerSettings.Offset;
             transform.position = Vector3.Lerp(transform.position, targetPosition, _cameraControllerSettings.LerpValue);
         }
     }    
